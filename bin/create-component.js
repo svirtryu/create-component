7ce3da2c301803ec393f8fbe5c2f525a1ecdd697
Shaw Yu <syu@virtru.com>
2018-10-01 15:59:46 -0500

    Use templating to generate components with passed name

--- bin/create-component.js
+++ bin/create-component.js
@@ -3,18 +3,19 @@
 const fs = require('fs');
 const path = require('path');
 const program = require('commander');
+const mustache = require('mustache');
 
 const { createFile } = require('../src/utils/files');
-const jsTemplate = fs.readFileSync(path.resolve(__dirname, '../src/templates/default-jsx.template'));
-const cssTemplate = fs.readFileSync(path.resolve(__dirname, '../src/templates/default-css.template'));
-const testTemplate = fs.readFileSync(path.resolve(__dirname, '../src/templates/default-test.template'));
+const jsTemplate = fs.readFileSync(path.resolve(__dirname, '../src/templates/default-jsx.template')).toString();
+const cssTemplate = fs.readFileSync(path.resolve(__dirname, '../src/templates/default-css.template')).toString();
+const testTemplate = fs.readFileSync(path.resolve(__dirname, '../src/templates/default-test.template')).toString();
 
 const componentName = process.argv[2];
 
 if (componentName) {
-  createFile(componentName, 'js', jsTemplate);
-  createFile(componentName, 'css', cssTemplate);
-  createFile(componentName, 'test.js', testTemplate);
+  createFile(componentName, 'js', mustache.render(jsTemplate, { componentName }));
+  createFile(componentName, 'css', mustache.render(cssTemplate, { componentName }));
+  createFile(componentName, 'test.js', mustache.render(testTemplate, { componentName }));
 } else {
   console.log('Usage: create-component <ComponentName>');
 }
